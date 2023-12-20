45b4cc2ac6b33c0c591c72a90ae4f8dfd7b3de2b
Manibharathi <manibharath159@gmail.com>
2023-12-20 18:49:18 +0530

    Initial commit

---
+++ Javascripts/script.js
@@ -0,0 +1,15 @@
+//alert("Welcome buddy!");
+// Create a Variable
+
+let myName = "jack";
+console.log("My Name",myName);
+
+let age = 35;
+console.log("My age is ",age);
+
+let fNumber = 45;
+let sNumber = " Praba";
+let result = fNumber+sNumber;
+console.log("Result is ",result);
+
+
